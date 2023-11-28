e9f0043bae8ebd574c4127af4ff99f7997e89d1e
ezerssss <magbanuaezra@gmail.com>
2023-11-28 14:05:52 +0800

    [Setup][Ezra] setup

---
+++ app/App.java
@@ -0,0 +1,10 @@
+package icebase.app;
+
+import icebase.icebase.Icebase;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Frontend app");
+        Icebase ib = new Icebase();
+    }
+}
---
+++ icebase/Icebase.java
@@ -0,0 +1,7 @@
+package icebase.icebase;
+
+public class Icebase {
+    public Icebase() {
+        System.out.println("Instantiated Icebase service.");
+    }
+}
