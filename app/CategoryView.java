54296fe259ffd5eed2f39b3e60707aeef811d300
unknown <ajtajaja13@gmail.com>
2023-12-11 00:09:39 +0800

    [Adjustment][Andry] implemented pr comments, adjusted display store name padding in menu title class

--- app/CategoryView.java
+++ app/CategoryView.java
@@ -9,31 +9,32 @@ public class CategoryView {
         Icebase ib = new Icebase();
         Scanner sc = new Scanner(System.in);
         int choice;
-        String storeName = "STORE X"; //fetch from icebase
-        String categoryName = "CATEGORY X";//fetch from icebase
-        String[] categoryList = {"Category A", "Category B", "Category C"}; //fetch from icebase
+        String storeName = "STORE X"; // stand-in value, should be set from constructor
+        String[] categoryList = { "Category A", "Category B", "Category C" }; // stand-in list
         int range = categoryList.length;
 
-        while (true){
-            System.out.printf("\t\t\t ---------%n\t\t\t| %s |%n\t\t\t ---------%n", storeName);
-            MenuTitle.displaySubTitle("[" + categoryName + "]");
-            for(int i = 0; i < range; i++){
+        while (true) {
+            MenuTitle.displayStoreName(storeName);
+            MenuTitle.displaySubTitle("[CATEGORIES]");
+            for (int i = 0; i < range; i++) {
                 System.out.printf(
-                                    "\t\t[%d] - %s%n", i+1, categoryList[i]);
+                        "\t\t[%d] - %s%n", i + 1, categoryList[i]);
             }
-            System.out.printf("\t\t[%d] - Return%n", range+1);
+            System.out.printf("\t\t[%d] - Return%n", range + 1);
 
             System.out.print("\nChoice: ");
-            try{
+            try {
                 choice = Integer.parseInt(sc.nextLine());
-                if(choice == range+1) break;
-                String category = categoryList[choice-1]; //fetch from icebase
-                System.out.println(category);//go to item list
-            }catch (Exception e){
+                if (choice == range + 1) {
+                    break;
+                }
+                String category = categoryList[choice - 1]; // fetch from icebase
+                System.out.println(category);// go to item list
+            } catch (Exception e) {
                 System.out.println("Please choose from the given options...\n");
             }
         }
-        
+
         sc.close();
     }
 }
--- app/ItemList.java
+++ app/ItemList.java
@@ -6,42 +6,45 @@ import java.util.Scanner;
 import icebase.icebase.Icebase;
 
 public class ItemList {
-    ItemList(){
+    ItemList() {
 
     }
 
-    public static void buyItem(String item){
+    public static void buyItem(String item) {
         System.out.println(item);
-        //display other item attributes
+        // display other item attributes
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Icebase ib = new Icebase();
         Scanner sc = new Scanner(System.in);
         int choice;
-        String storeName = "STORE X";
-        String categoryName = "CATEGORY X";
-        String[] itemList = {"Item A", "Item B", "Item C"}; //fetch from icebase
+        String storeName = "STORE X"; // stand-in value, should be set from constructor
+        String categoryName = "CATEGORY X"; // stand-in value, should be set from constructor
+        String[] itemList = { "Item A", "Item B", "Item C" }; // fetch from icebase
         int range = itemList.length;
-        while (true){
-            System.out.printf("\t\t\t ---------%n\t\t\t| %s |%n\t\t\t ---------%n", storeName);
+        while (true) {
+            MenuTitle.displayStoreName(storeName);
             MenuTitle.displaySubTitle("[" + categoryName + "]");
-            for(int i = 0; i < range; i++){
+            for (int i = 0; i < range; i++) {
                 System.out.printf(
-                                    "\t\t[%d] - %s%n", i+1, itemList[i]);
+                        "\t\t[%d] - %s%n", i + 1, itemList[i]);
             }
-            System.out.printf("\t\t[%d] - Return%n", range+1);
+            System.out.printf("\t\t[%d] - Return%n", range + 1);
 
             System.out.print("\nChoice: ");
-            try{
+            try {
                 choice = Integer.parseInt(sc.nextLine());
-                if(choice == range+1) break;
-                String item = itemList[choice-1]; //fetch from icebase
+                if (choice == range + 1) {
+                    break;
+                }
+                String item = itemList[choice - 1]; // fetch from icebase
                 buyItem(item);
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Please choose from the given options...\n");
             }
         }
-       
+
         sc.close();
     }
 }
