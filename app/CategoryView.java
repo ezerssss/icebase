d24baa5eb0df9103b500e144a6adac1993b0f31c
unknown <ajtajaja13@gmail.com>
2023-12-10 16:04:34 +0800

    [Screen][Andry] Category view, Item list

--- app/CategoryView.java
+++ app/CategoryView.java
@@ -8,17 +8,32 @@ public class CategoryView {
     public static void main(String[] args) {
         Icebase ib = new Icebase();
         Scanner sc = new Scanner(System.in);
-
+        int choice;
+        String storeName = "STORE X"; //fetch from icebase
+        String categoryName = "CATEGORY X";//fetch from icebase
         String[] categoryList = {"Category A", "Category B", "Category C"}; //fetch from icebase
+        int range = categoryList.length;
 
-        MenuTitle.displayMainTitle();
-        MenuTitle.displaySubTitle("[CATEGORIES]");
-        for(int i = 0; i < categoryList.length; i++){
-            System.out.printf(
-                                "\t\t[%d] - %s%n", i+1, categoryList[i]);
+        while (true){
+            System.out.printf("\t\t\t ---------%n\t\t\t| %s |%n\t\t\t ---------%n", storeName);
+            MenuTitle.displaySubTitle("[" + categoryName + "]");
+            for(int i = 0; i < range; i++){
+                System.out.printf(
+                                    "\t\t[%d] - %s%n", i+1, categoryList[i]);
+            }
+            System.out.printf("\t\t[%d] - Return%n", range+1);
+
+            System.out.print("\nChoice: ");
+            try{
+                choice = Integer.parseInt(sc.nextLine());
+                if(choice == range+1) break;
+                String category = categoryList[choice-1]; //fetch from icebase
+                System.out.println(category);//go to item list
+            }catch (Exception e){
+                System.out.println("Please choose from the given options...\n");
+            }
         }
-        System.out.print("\nChoice: ");
-        sc.nextLine();
+        
         sc.close();
     }
 }
--- app/ItemList.java
+++ app/ItemList.java
@@ -1,5 +1,6 @@
 package icebase.app;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import icebase.icebase.Icebase;
@@ -8,20 +9,39 @@ public class ItemList {
     ItemList(){
 
     }
+
+    public static void buyItem(String item){
+        System.out.println(item);
+        //display other item attributes
+    }
     public static void main(String[] args){
         Icebase ib = new Icebase();
         Scanner sc = new Scanner(System.in);
+        int choice;
+        String storeName = "STORE X";
+        String categoryName = "CATEGORY X";
         String[] itemList = {"Item A", "Item B", "Item C"}; //fetch from icebase
+        int range = itemList.length;
+        while (true){
+            System.out.printf("\t\t\t ---------%n\t\t\t| %s |%n\t\t\t ---------%n", storeName);
+            MenuTitle.displaySubTitle("[" + categoryName + "]");
+            for(int i = 0; i < range; i++){
+                System.out.printf(
+                                    "\t\t[%d] - %s%n", i+1, itemList[i]);
+            }
+            System.out.printf("\t\t[%d] - Return%n", range+1);
 
-        MenuTitle.displayMainTitle();
-        MenuTitle.displaySubTitle("[CATEGORY X]");
-        for(int i = 0; i < itemList.length; i++){
-            System.out.printf(
-                                "\t\t[%d] - %s%n", i+1, itemList[i]);
+            System.out.print("\nChoice: ");
+            try{
+                choice = Integer.parseInt(sc.nextLine());
+                if(choice == range+1) break;
+                String item = itemList[choice-1]; //fetch from icebase
+                buyItem(item);
+            }catch (Exception e){
+                System.out.println("Please choose from the given options...\n");
+            }
         }
-
-        System.out.print("\npress enter to continue...");
-        sc.nextLine();
+       
         sc.close();
     }
 }
