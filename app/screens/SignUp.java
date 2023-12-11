c259b9252c75d7be16d8c50824c1ce0c283da743
Rian Mabait <125614016+rianluisx@users.noreply.github.com>
2023-12-12 03:03:33 +0800

    Update SignUp.java

--- app/screens/SignUp.java
+++ app/screens/SignUp.java
@@ -1,10 +1,45 @@
 package icebase.app.screens;
 
 import icebase.app.App;
+import icebase.app.MenuTitle;
+import icebase.icebase.Auth;
+import icebase.icebase.exceptions.InvalidUsernameException;
+
+import java.io.IOException;
 import java.util.Scanner;
 
 public class SignUp implements Screen {
     public void display() {
         Scanner sc = App.sc;
+        while (true) {
+            MenuTitle.displayMainTitle();
+            MenuTitle.displaySubTitle("[Sign-up]");
+
+            try {
+                Auth auth = Auth.getAuth();
+
+                System.out.print("Enter a new username: ");
+                String username = sc.nextLine();
+
+                System.out.print("Enter a new password: ");
+                String password = sc.nextLine();
+
+                // handles exceptions
+                try {
+                    auth.signUp(username, password);
+                    System.out.println("Sign-up successful!");
+                    break; 
+                } catch (InvalidUsernameException e) {
+                    System.out.println("Username already exists. Please choose a different username.");
+                } catch (IOException e) {
+                    System.out.println("Error occurred while signing up. Please try again.");
+                    e.printStackTrace(); 
+                }
+
+            } catch (Exception e) {
+                System.out.println("An unexpected error occurred. Please try again.");
+                e.printStackTrace(); 
+            }
+        }
     }
 }
