057e2d258fdea1131a80acd9a5a80d524fa1a449
Jhoanna Olana <113810517+jrolana@users.noreply.github.com>
2023-12-12 17:25:22 +0800

    [Screen] StoreList

--- app/screens/home/StoreList.java
+++ app/screens/home/StoreList.java
@@ -1,5 +1,6 @@
 package icebase.app.screens.home;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import icebase.app.App;
 import icebase.app.MenuTitle;
@@ -8,12 +9,37 @@ import icebase.app.screens.Screen;
 public class StoreList implements Screen {
     public void display() {
         Scanner sc = App.sc;
+        int choice;
+        ArrayList<String> storeNames = new ArrayList<String>();
 
-        MenuTitle.displayMainTitle();
-        MenuTitle.displaySubTitle("[STORES]");
-        System.out.println(
-                "\t\tStore 1\n\t\tStore 2\n\t\t...\n");
-        System.out.print("Choice: ");
-        sc.nextLine();
+        ArrayList<String> stores = new ArrayList<String>(); // fecth from icebase (type should be Stores)
+        stores.add("Store A"); // stand-in value
+        stores.add("Store B");
+        stores.add("Store C");
+
+        for (String store : stores) { // String -> Store
+            storeNames.add(store); // store -> store.getName()
+        }
+        storeNames.add("Return");
+
+        int range = storeNames.size();
+
+        while (true) {
+            MenuTitle.displayMainTitle();
+            MenuTitle.displaySubTitle("[STORES]");
+            // MenuTitle.displayOptions(storeNames);
+
+            try {
+                System.out.print("Choice: ");
+                choice = Integer.parseInt(sc.nextLine());
+                if (choice == range + 1) {
+                    return;
+                }
+                System.out.println(storeNames.get(choice));
+                // BuyingView(stores.get(choice));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
