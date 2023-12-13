ecc5e95e594e2bf006f012ced51f4c2e7735eef0
Rian Mabait <125614016+rianluisx@users.noreply.github.com>
2023-12-13 11:11:55 +0800

    [Sign-up]  [Catched Errors] [Rian]

--- app/screens/SignUp.java
+++ app/screens/SignUp.java
@@ -1,5 +1,4 @@
 package icebase.app.screens;
-
 import icebase.app.App;
 import icebase.app.MenuTitle;
 import icebase.app.api.API;
@@ -27,18 +26,52 @@ public class SignUp implements Screen {
 
             // handles exceptions
             try {
+
                 User user = auth.signUp(username, password);
 
-                System.out.print("Enter your store name: ");
-                String storeName = sc.nextLine();
+                System.out.println();
+                MenuTitle.displayMainTitle();
+                MenuTitle.displaySubTitle("[Set-up]");
 
-                System.out.print("Enter your starting money: ");
-                Double money = Double.parseDouble(sc.nextLine());
+                String storeName = null;
+                while (storeName == null || storeName.isEmpty()) {
+                    System.out.print("Enter your store name: ");
+                    storeName = sc.nextLine();
 
-                API.setupUser(user, storeName, money);
+                    if (storeName.isEmpty()) {
+                        System.out.println("\nStore name cannot be empty. Please enter a valid store name.");
+                    }
+                }
+
+                Double startingMoney = null;
+                while (startingMoney == null || startingMoney < 0) {
+                    try {
+                        System.out.print("\nEnter your starting money: ");
+                        String moneyInputStr = sc.nextLine();
+                
+                        if (moneyInputStr.isEmpty()) {
+                            System.out.println("Starting money cannot be empty. Please enter a valid amount.");
+                            continue;
+                        }
+                        
+                        // parse to double to check if negative
+                        double moneyInput = Double.parseDouble(moneyInputStr);
+                
+                        if (moneyInput < 0) {
+                            System.out.println("Starting money cannot be negative. Please enter a positive amount.");
+                            continue;
+                        }
+                        startingMoney = moneyInput;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input. Please enter a valid numeric value for starting money.");
+                    }
+                }
+                
+                API.setupUser(user, storeName, startingMoney);
 
                 System.out.println("Sign-up successful!");
                 break;
+                
             } catch (InvalidUsernameException e) {
                 System.out.println("Username already exists. Please choose a different username.");
             } catch (IOException e) {
