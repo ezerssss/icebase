a8ca1c9d9fb979599df1f7ce401e35ca2df87a12
Rian Mabait <125614016+rianluisx@users.noreply.github.com>
2023-12-13 14:57:18 +0800

    [Sign-up] [Rian]

--- app/screens/SignUp.java
+++ app/screens/SignUp.java
@@ -1,7 +1,10 @@
 package icebase.app.screens;
+
 import icebase.app.App;
 import icebase.app.MenuTitle;
+import icebase.app.Router;
 import icebase.app.api.API;
+import icebase.app.enums.SCREEN_ENUM;
 import icebase.icebase.Auth;
 import icebase.icebase.User;
 import icebase.icebase.exceptions.InvalidUsernameException;
@@ -13,65 +16,26 @@ public class SignUp implements Screen {
     public void display() {
         Scanner sc = App.sc;
         while (true) {
-            MenuTitle.displayMainTitle();
-            MenuTitle.displaySubTitle("Sign-up");
-
-            Auth auth = Auth.getAuth();
-
-            System.out.print("Enter a new username: ");
-            String username = sc.nextLine();
-
-            System.out.print("Enter a new password: ");
-            String password = sc.nextLine();
-
             // handles exceptions
             try {
+                MenuTitle.displayMainTitle();
+                MenuTitle.displaySubTitle("Sign-up");
+
+                Auth auth = Auth.getAuth();
+
+                System.out.print("Enter a new username: ");
+                String username = sc.nextLine();
+
+                System.out.print("Enter a new password: ");
+                String password = sc.nextLine();
 
                 User user = auth.signUp(username, password);
 
-                System.out.println();
-                MenuTitle.displayMainTitle();
-                MenuTitle.displaySubTitle("[Set-up]");
-
-                String storeName = null;
-                while (storeName == null || storeName.isEmpty()) {
-                    System.out.print("Enter your store name: ");
-                    storeName = sc.nextLine();
-
-                    if (storeName.isEmpty()) {
-                        System.out.println("\nStore name cannot be empty. Please enter a valid store name.\n");
-                    }
-                }
-
-                Double startingMoney = null;
-                while (startingMoney == null || startingMoney < 0) {
-                    try {
-                        System.out.print("\nEnter your starting money: ");
-                        String moneyInputStr = sc.nextLine();
-                
-                        if (moneyInputStr.isEmpty()) {
-                            System.out.println("Starting money cannot be empty. Please enter a valid amount.");
-                            continue;
-                        }
-                        
-                        // parse to double to check if negative
-                        double moneyInput = Double.parseDouble(moneyInputStr);
-                
-                        if (moneyInput < 0) {
-                            System.out.println("Starting money cannot be negative. Please enter a positive amount.");
-                            continue;
-                        }
-                        startingMoney = moneyInput;
-                    } catch (NumberFormatException e) {
-                        System.out.println("Invalid input. Please enter a valid numeric value for starting money.");
-                    }
-                }
-                
-                API.setupUser(user, storeName, startingMoney);
+                Router.navigate(SCREEN_ENUM.SET_UP, user);
 
                 System.out.println("Sign-up successful!");
                 break;
-                
+
             } catch (InvalidUsernameException e) {
                 System.out.println("Username already exists. Please choose a different username.");
             } catch (IOException e) {
